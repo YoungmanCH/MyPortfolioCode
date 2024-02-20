afee146e3f1d65fb140c950bff49965303ab0c60
Youngman <youngman@YoungmannoMacBook-Air.local>
2024-02-20 21:17:23 +0900

    modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? 'https://youngmanch.github.io/MyPortfolio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/MyPortfolio/' : '/',
   plugins: [react()],
   assetsInclude: ['**/*.glb']
-})
+});
+
