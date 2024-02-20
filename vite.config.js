b309be63ebcaee4d968b3a9b6675ce970c5ee1f0
Youngman <youngman@YoungmannoMacBook-Air.local>
2024-02-20 21:14:04 +0900

    modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? 'https://youngmanch.github.io/MyPortfolio/' : '/',
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
